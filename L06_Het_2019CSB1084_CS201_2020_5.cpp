c9c3438de27fd81618736aedd18e0fb7370bf4d4
Het <62541263+HETFADIA@users.noreply.github.com>
2020-12-14 14:48:38 +0530

    Add files via upload

---
+++ L06_Het_2019CSB1084_CS201_2020_5.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long
+#define lld long long
+#define p(x) cout << x << "\n";
+#define p2(x, y) cout << x << " " << y << "\n";
+#define p3(x, y, z) cout << x << " " << y << " " << z << "\n";
+#define ps(x) cout << x << " ";
+#define ps2(x, y) cout << x << " " << y << " ";
+#define ps3(x, y, z) cout << x << " " << y << " " << z << " ";
+#define ps4(x, y, z, a) cout << x << " " << y << " " << z << " " << a << " ";
+#define ps5(x, y, z, a, b) cout << x << " " << y << " " << z << " " << a << " " << b << " ";
+#define pns(x) cout << x;
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define dll(x) x.begin(), x.end(), greater<lld>()
+#define vect(x, n) vector<lld> x(n, 0);
+#define matrix(x, n, m) vector<vector<lld>> x(n, vector<lld>(m, 0));
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define lb lower_bound
+#define ub upper_bound
+#define pb(x) push_back(x);
+#define pf(x) push_front(x);
+#define mkp(x, y) make_pair(x, y)
+#define inf 1e14
+signed main()
+{
+}
